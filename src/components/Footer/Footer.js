7a6a5f51b4322ae0108fb21e6e5488624ff32ff2
ahmed3520 <ataha352000@gmail.com>
2021-06-27 21:33:58 +0200

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,18 +4,21 @@ import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
 
+
 const Footer = () => {
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
   return (
     <FooterWrapper>
       <LinkList>
         <LinkColumn>
           <LinkTitle>Call</LinkTitle>
-          <LinkItem href="tel:314-343-3432">314-343-3432</LinkItem>
+          <LinkItem href="tel:+201004581592">201004581592</LinkItem>
         </LinkColumn>
         <LinkColumn>
           <LinkTitle>Email</LinkTitle>
-          <LinkItem href="mailto:contact@jsmastery.com">
-            contact@jsmastery.com
+          <LinkItem href="mailto:ataha352000@gmail.com">
+           ataha352000@gmail.com
           </LinkItem>
         </LinkColumn>
       </LinkList>
@@ -24,13 +27,13 @@ const Footer = () => {
           <Slogan>Innovating one project at a time</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href="https://google.com">
+          <SocialIcons href="https://github.com/ahmed3520">
             <AiFillGithub size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://google.com">
+          <SocialIcons href="https://www.linkedin.com/in/ahmed-taha-0187ba19a/">
             <AiFillLinkedin size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://google.com">
+          <SocialIcons href="https://www.instagram.com/_ahmed352_/">
             <AiFillInstagram size="3rem" />
           </SocialIcons>
         </SocialContainer>
